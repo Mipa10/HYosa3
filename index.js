b5a8db0f3f4819a7c58b5107432707799e37dbd3
Mipa <mikko.paivikko@gmail.com>
2020-10-04 16:35:08 +0300

    osa3 eka commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Aada Lovelace",
+    number: "432543254325",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "040-2342436",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "04032562576456",
+  },
+];
+
+app.get("/info", (req, res) => {
+  res.send(`<p>phonebook has info of ${notes.length} people<p>
+    <p>${new Date()}</p>`);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.send(notes);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = notes.find((note) => note.id === id);
+  if (person) {
+    return res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons/", (req, res) => {
+  if (!req.body.name || !req.body.number) {
+    return res.status(404).json({
+      error: "name and number required",
+    });
+  }
+  if(notes.some(person => person.name === req.body.name)) {
+    return res.status(404).json({
+        error: "name must be unique",
+      });
+  }
+  const id = Math.floor(Math.random() * 1000);
+
+  const newPerson = {
+    id: id,
+    name: req.body.name,
+    number: req.body.number,
+  };
+
+  notes = notes.concat(newPerson);
+  res.json(newPerson);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
