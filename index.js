0710f3aa4ba6871d41d7f656270b6afc55356376
Mipa <mikko.paivikko@gmail.com>
2020-10-06 21:09:53 +0300

    3.18 backend

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 //const { response } = require("express");
 const express = require("express");
 const morgan = require("morgan");
-const cors =require('cors')
-const Person = require('./mongo');
-const mongoose = require('mongoose');
-
+const cors = require("cors");
+const Person = require("./mongo");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 morgan.token("info", function getInfo(req) {
   const info = JSON.stringify(req.body);
@@ -44,71 +43,119 @@ let notes = [
   },
 ];
 
-app.get("/info", (req, res) => {
-  res.send(`<p>phonebook has info of ${notes.length} people<p>
+app.get("/info", (req, res, next) => {
+  const count = Person.countDocuments()
+  .then(response => {
+    console.log(response)
+
+    
+    res.send(`<p>phonebook has info of ${response} people<p>
     <p>${new Date()}</p>`);
+
+  })
+ 
+  
+  
 });
 
-app.get("/api/persons", (req, res) => {
-  let ihmiset = []
-  Person.find({}).then(response => {
-    console.log('person', response)
-    //console.log('ressi', res.json(response))
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((response) => {
+      console.log("person", response);
+      //console.log('ressi', res.json(response))
 
-    res.json(response)
-   
-    //mongoose.connection.close()
-   // console.log(ihmiset)
-    
-    
-  })
+      res.json(response);
+
+      //mongoose.connection.close()
+      // console.log(ihmiset)
+    })
+    .catch((error) => {
+      next(error)
+    });
   //res.send(notes);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+  .then(result => {
+    console.log(result)
+    
+    
+    res.json(result)
+  })
+  .catch(error => {
+    next(error)
+  })
 
-  const person = notes.find((note) => note.id === id);
-  if (person) {
-    return res.json(person);
-  } else {
-    res.status(404).end();
+
+});
+
+app.put('/api/persons/:id', (req,res,next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number
   }
+  Person.findByIdAndUpdate(req.params.id, person, {new : true} )
+  .then(updatedPerson => {
+    
+    
+    res.json(updatedPerson)
+  })
+  .catch(error => {
+    next(error)
+  })
+})
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error)
+    });
+  // const id = Number(req.params.id);
+  // notes = notes.filter((note) => note.id !== id);
+
+  // res.status(204).end();
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  res.status(204).end();
-});
-
-app.post("/api/persons/", (req, res) => {
+app.post("/api/persons/", (req, res, next) => {
   if (!req.body.name || !req.body.number) {
     return res.status(404).json({
       error: "name and number required",
     });
   }
-  if (notes.some((person) => person.name === req.body.name)) {
-    return res.status(404).json({
-      error: "name must be unique",
-    });
-  }
-  
+  // if (notes.some((person) => person.name === req.body.name)) {
+  //   return res.status(404).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
   const newPerson = new Person({
-    
     name: req.body.name,
     number: req.body.number,
   });
 
-  newPerson.save().then(result => {
-    console.log('person saved!')
-    mongoose.connection.close()
-    
-  })
+  newPerson
+    .save()
+    .then((result) => {
+      console.log("person saved!");
+      //mongoose.connection.close()
+    })
+    .catch((error) => {
+      next(error)
+    });
   res.json(newPerson);
 });
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  next(error)
+  
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 
